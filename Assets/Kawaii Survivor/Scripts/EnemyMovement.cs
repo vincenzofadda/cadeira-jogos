3dde84f6ce729c908dd9ed68a4f6d23ae66a20dc
vincenzofadda <vincenzofadda@gmail.com>
2025-02-06 08:39:13 -0300

    feat: add Enemy Movement script

---
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{   
+    [Header("Elements")]
+    private Player player;
+
+    [Header("Setting")]
+    [SerializeField] private float moveSpeed;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = FindFirstObjectByType<Player>();
+
+        if(player == null)
+        {
+          Debug.LogWarning("Não há nenhum jogador, destruindo inimigos");
+          Destroy(gameObject);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+
+        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
+
+        transform.position = targetPosition;
+
+    }
+}
