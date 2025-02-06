0f27e67dbc91f32a9f992b5724f7cf4175d676d7
Vincenzo Fadda <vincenzofadda@gmail.com>
2025-02-06 15:42:29 -0300

    refactor: EnemyMovement and Player scripts

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -1,39 +1,31 @@
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
-{   
+{
     [Header("Elements")]
     private Player player;
-
-    [Header("Setting")]
+    
+    [Header("Settings")]
     [SerializeField] private float moveSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    {   
         player = FindFirstObjectByType<Player>();
 
         if(player == null)
         {
-          Debug.LogWarning("Não há nenhum jogador, destruindo inimigos");
-          Destroy(gameObject);
-
+            Debug.LogWarning("Player não detectado, destruindo inimigos..");
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        FollowPlayer();
+       Vector2 direction = (player.transform.position - transform.position).normalized;
+
+       Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime; 
+
+       transform.position = targetPosition;
     }
-
-    private void FollowPlayer()
-    {
-        Vector2 direction = (player.transform.position - transform.position).normalized;
-
-        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime;
-
-        transform.position = targetPosition;
-    }
-
-
 }
