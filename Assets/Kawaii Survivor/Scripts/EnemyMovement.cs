6f56a0ddb59443356c7967dba13c8804c14dec19
Vincenzo Fadda <vincenzofadda@gmail.com>
2025-02-07 11:27:56 -0300

    refactor: enemy movement and attack logic

--- Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
+++ Assets/Kawaii Survivor/Scripts/EnemyMovement.cs
@@ -4,71 +4,20 @@ public class EnemyMovement : MonoBehaviour
 {
     [Header("Elements")]
     private Player player;
-    
-    [Header("Spawn Sequence Related")]
-    [SerializeField] private SpriteRenderer renderer;
-    [SerializeField] private SpriteRenderer spawnIndicator;
-    private bool hasSpawned;
-    
+
     [Header("Settings")]
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float playerDetectionRadius;
-
-    [Header("Effects")]
-    [SerializeField] private ParticleSystem deathParticles;
-
-    [Header("Attack")]
-    [SerializeField] private int damage;
-    [SerializeField] private float attackFrequency;
-    private float attackDelay;
-    private float attackTimer;
-
-    [Header("DEBUG")]
-    [SerializeField] private bool gizmos;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {   
-        player = FindFirstObjectByType<Player>();
-
-        if(player == null)
-        {
-            Debug.LogWarning("Player não detectado, destruindo inimigos..");
-        }
-
-        renderer.enabled = false;
-        spawnIndicator.enabled =  true;
-        Vector3 targetScale = spawnIndicator.transform.localScale * 1.2f;
-        LeanTween.scale(spawnIndicator.gameObject, targetScale, .3f).setLoopPingPong(4).setOnComplete(SpawnSequenceCompleted);
-
-        attackDelay = 1f / attackFrequency;
-    }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(!hasSpawned)
-            return;
-        FollowPlayer();
-
-        if(attackTimer >= attackDelay)
-        {
-            TryAttack();
-        }   
-        else
-        {
-            Wait();
-        }        
-        
+        if(player != null)
+            FollowPlayer();
     }
 
-    private void SpawnSequenceCompleted()
+    public void storePlayer(Player player)
     {
-        renderer.enabled = true;
-        spawnIndicator.enabled =  false;
-
-        hasSpawned = true;
+        this.player = player;
     }
 
     private void FollowPlayer()
@@ -78,43 +27,5 @@ public class EnemyMovement : MonoBehaviour
        Vector2 targetPosition = (Vector2)transform.position + direction * moveSpeed * Time.deltaTime; 
 
        transform.position = targetPosition;
-    }
-
-    private void TryAttack()
-    {
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-
-        if(distanceToPlayer <= playerDetectionRadius)
-        {
-            Attack();
-            
-        }
-    }
-
-    private void Wait()
-    {
-        attackTimer += Time.deltaTime; 
-    }
-
-    private void Attack()
-    {
-        Debug.Log("Jogador tomou " + damage + " de dano!");
-        attackTimer = 0;
-    }
-
-    private void Death()
-    {
-        deathParticles.transform.SetParent(null);
-        deathParticles.Play();
-        Destroy(gameObject);
-    }
-
-    private void OnDrawGizmos()
-    {
-        if(!gizmos)
-            return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, playerDetectionRadius);
     }
 }
